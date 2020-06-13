51de6f8bc01ad3cc0770f08ccb16f05956a612f6
David Pedroza <davidpedroza@gmail.com>
2020-06-13 10:48:18 -0300

    create presentation module

--- app/src/main/java/com/marvel/presentation/CharactersActivity.kt
+++ app/src/main/java/com/marvel/presentation/CharactersActivity.kt
@@ -1,11 +1,12 @@
-package com.marvel
+package com.marvel.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.marvel.R
 
-class MainActivity : AppCompatActivity() {
+class CharactersActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_characters)
     }
 }
