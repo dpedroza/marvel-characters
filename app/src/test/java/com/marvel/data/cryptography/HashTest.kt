471ff46511e750c5ce779d602ee9659af20dff3c
David Pedroza <davidpedroza@gmail.com>
2020-06-20 14:30:41 -0300

    MD5 testing

---
+++ app/src/test/java/com/marvel/data/cryptography/HashTest.kt
@@ -0,0 +1,19 @@
+package com.marvel.data.cryptography
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class HashTest {
+
+    @Test
+    fun assertMD5GenerationBehavior() {
+        val timestamp = "TIMESTAMP"
+        val publicKey = "API_KEY"
+        val privateKey = "PRIVATE_KEY"
+        val expected = "1f965bd78d49b510ff6342e9f41e49bc"
+        val answer = Hash.generateMD5(timestamp, publicKey, privateKey)
+
+        assertEquals(expected, answer)
+    }
+
+}
