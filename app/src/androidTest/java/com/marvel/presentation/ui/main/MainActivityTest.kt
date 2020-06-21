3c063f104b3c5d745f701a7adf3547d214010a56
David Pedroza <davidpedroza@gmail.com>
2020-06-21 14:43:42 -0300

    create tests for main activity

--- app/src/androidTest/java/com/marvel/presentation/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/marvel/presentation/ui/main/MainActivityTest.kt
@@ -1,7 +1,13 @@
 package com.marvel.presentation.ui.main
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.swipeLeft
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import com.marvel.R
+import org.hamcrest.core.IsNot.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,13 +17,21 @@ class MainActivityTest {
 
     @Rule
     @JvmField
-    var activityRule: ActivityTestRule<MainActivity> =
-        ActivityTestRule(MainActivity::class.java, true, false)
+    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun onSuccessState_assertResultIsDisplayed() {
+    fun assertInitialState() {
+        onView(withText("Personagens")).check(matches(isDisplayed()))
+        onView(withText("Favoritos")).check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_character)).check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_favorite)).check(matches(not(isDisplayed())))
+        onView(withText("Favoritos")).check(matches(isDisplayed()))
+    }
 
-        activityRule.launchActivity(null)
-
+    @Test
+    fun onLeftSwipeShouldShowFavoritesTab() {
+        onView(withId(R.id.viewPager)).perform(swipeLeft())
+        onView(withId(R.id.fragment_character)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.fragment_favorite)).check(matches(isDisplayed()))
     }
 }
